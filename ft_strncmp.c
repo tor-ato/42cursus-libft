2c2f55e3a9de7e1c4b4cc71c2a23207e304cf5ab
Torato Kitahara <tkitahar@c1r16s6.42tokyo.jp>
2024-04-22 21:54:01 +0900

    ft_strncmp OK

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,10 +6,36 @@
 /*   By: tkitahar <tkitahar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 18:39:46 by tkitahar          #+#    #+#             */
-/*   Updated: 2024/04/22 21:01:22 by tkitahar         ###   ########.fr       */
+/*   Updated: 2024/04/22 21:53:17 by tkitahar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	while ((*s1 || *s2) && n != 0)
+	{
+		if (!(*s1 == *s2))
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+		n--;
+	}
+	return (0);
+}
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int	main(void)
+// {
+// 	char *test1s1;
+// 	test1s1 = "12345";
+// 	char *test1s2;
+// 	test1s2 = "12a45";
+// 	int test1;
+// 	test1 = 3;
+// 	printf("origin\t%d\n", strncmp(test1s1, test1s2, test1));
+// 	printf("ft\t%d\n", ft_strncmp(test1s1, test1s2, test1));
+// }
