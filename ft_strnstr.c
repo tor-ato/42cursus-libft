3d7b78ee1d2ff5bb98b5e5b73e1391d6576fe06c
Torato Kitahara <tkitahar@c1r16s6.42tokyo.jp>
2024-04-23 13:47:54 +0900

    ft_strnstr OK

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,10 +6,25 @@
 /*   By: tkitahar <tkitahar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 18:41:51 by tkitahar          #+#    #+#             */
-/*   Updated: 2024/04/21 20:18:34 by tkitahar         ###   ########.fr       */
+/*   Updated: 2024/04/23 13:44:37 by tkitahar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	while (*s1++)
+	{
+		while (*s2++)
+		{
+			if (ft_strncmp(s1, s2, n))
+			{
+				return ((int)s1);
+			}
+			else
+				return ((int)NULL);
+		}
+	}
+	return ((int)NULL);
+}
