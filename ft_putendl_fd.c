ee3a0a2a048b3dde2569b5a41780431bbff4c620
torato <torato@toratonoMacBook-Air.local>
2024-05-07 11:15:10 +0900

    tmp push

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -12,4 +12,16 @@
 
 #include "libft.h"
 
-void	ft_putendl_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd)
+{
+    return;
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("ft_putstr_fd start\n");
+    ft_putendl_fd("12345",1);
+    return 0;
+}
